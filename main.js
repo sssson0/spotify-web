4f19abe74994377f332835434d8aeea42ae0831f
sssson0 <s20023094@gmail.com>
2024-06-01 16:41:33 +0900

    “commit”

---
+++ main.js
@@ -0,0 +1,7 @@
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "300px";
+};
+
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+};
